138d9e5b1f1d6011e2a9ff0c7b4f5677e0642c1f
emillyalmeida <emillyalmeidan@gmail.com>
2022-06-30 14:09:32 -0300

    feat: create db and add sql

---
+++ sql/init.sql
@@ -0,0 +1,14 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS aplication_users(
+     uuid uuid DEFAULT uuid_generate_v4(),
+     username VARCHAR NOT NULL,
+     password VARCHAR NOT NULL,
+     PRIMARY KEY (uuid)
+ )
+
+INSERT INTO aplication_users (username, password) VALUES ('emilly', crypt('admin', 'my_salt'));
+
+
+
---
+++ src/db.ts
@@ -0,0 +1,6 @@
+import { Pool } from "pg";
+
+const connectionString =
+  "postgres://vnjgulsw:3mFa-ZqlCDkHD9xdcaZOLXDwqAiybzl_@motty.db.elephantsql.com/vnjgulsw";
+
+const db = new Pool({ connectionString });
