72302e2c5a5e9bc733c92a4f3c279045b5a5599e
emillyalmeida <emillyalmeidan@gmail.com>
2022-07-28 07:48:55 -0300

    feat: reorganize files and paths

--- src/Routes/usersRouter.ts
+++ src/Routes/usersRouter.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
-import ModelUser from "../models/modelUser";
+import ModelUser from "../controlers/modelUser";
 
 const userRoute = Router();
 
